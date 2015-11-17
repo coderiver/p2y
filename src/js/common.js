a72ba40fea9b950a4c6ad903256703ef9d6c0dad
valeraderevshukov <kaktuz34@gmail.com>
2015-11-17 18:09:27 +0200

    upd

--- src/js/common.js
+++ src/js/common.js
@@ -1,19 +1,32 @@
 $(document).ready(function() {
 	
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
-
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
-
-	console.log($('body').html());
+	// accordion
+	function accord() {
+		$('.js-accord').each(function() {
+			var this_ 	= $(this),
+				block 	= this_.find('.js-accord-block');
+			if (this_.hasClass('is-active')) {
+				block.show();
+			}
+		});
+		$('.js-accord-but').on('click', function() {
+			var this_ 		= $(this),
+				parent 		= this_.parents('.js-accord'),
+				blockThis 	= parent.find('.js-accord-block');
+				accord 		= $('.js-accord'),
+				block 		= accord.find('.js-accord-block');
+			if (!parent.hasClass('is-active')) {
+				accord.removeClass('is-active');
+				block.slideUp(300);
+				parent.addClass('is-active');
+				blockThis.slideDown(300);
+			}
+			else {
+				parent.removeClass('is-active');
+				blockThis.slideUp(300);
+			}
+			return false;
+		});
+	} accord();
+	
 });
