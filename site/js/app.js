fd8de88b0dcfc4b09d9e9f633789557d31658a68
valeraderevshukov <kaktuz34@gmail.com>
2015-11-18 18:37:09 +0200

    upd

--- site/js/app.js
+++ site/js/app.js
@@ -51,5 +51,27 @@ $(document).ready(function() {
 			return false;
 		});
 	} accord();
+
+	// tab
+	function tab() {
+		$(".js-tab").each(function(){
+			var tab_link 	= $(this).find("a"),
+				tab_item 	= $(this).find("li"),
+				index 		= tab_link.attr("href"),
+				parents 	= $(this).parents(".js-tab-group"),
+				tab_cont 	= parents.find(".js-tab-cont");
+			tab_link.on("click", function() {
+				var index = $(this).attr("href");
+				tab_item.removeClass("is-active");
+				$(this).parent().addClass("is-active");
+				tab_cont.fadeOut(0);
+				parents.find("."+index).fadeIn(300);
+				return false;
+			});
+			$(this).find('li:first').addClass("is-active");
+			parents.find("."+index).show();
+		});
+	}
+	tab();
 	
 });
