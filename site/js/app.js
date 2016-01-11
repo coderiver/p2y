999e72bc29eb9c4da196dd6cfd44671e153fa1dd
valeraderevshukov <kaktuz34@gmail.com>
2016-01-11 17:56:55 +0200

    upd

--- site/js/app.js
+++ site/js/app.js
@@ -96,26 +96,15 @@ $(document).ready(function() {
 		});
 	} accord();
 
-	// tab
-	function tab() {
-		$(".js-tab").each(function(){
-			var tab_link 	= $(this).find("a"),
-				tab_item 	= $(this).find("li"),
-				index 		= tab_link.attr("href"),
-				parents 	= $(this).parents(".js-tab-group"),
-				tab_cont 	= parents.find(".js-tab-cont");
-			tab_link.on("click", function() {
-				var index = $(this).attr("href");
-				tab_item.removeClass("is-active");
-				$(this).parent().addClass("is-active");
-				tab_cont.fadeOut(0);
-				parents.find("."+index).fadeIn(300);
-				return false;
-			});
-			$(this).find('li:first').addClass("is-active");
-			parents.find("."+index).show();
-		});
-	}
-	tab();
+	// card-focus
+	$('.js-card input').on('focus', function(){
+		var parent = $(this).parents('.js-card');
+		parent.addClass('is-active');
+	});
+	$('.js-card input').on('blur', function(){
+		var parent = $(this).parents('.js-card');
+		parent.removeClass('is-active');
+	});
+
 	
 });
